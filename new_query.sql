c6aa8164e4b3d2836aa60aedd444ad4383a94f39
khomenkoalx <a.khomenko42@gmail.com>
2025-07-14 10:11:54 +0300

    Исправлено условие фильтрации по id_operation_type

--- new_query.sql
+++ new_query.sql
@@ -31,23 +31,31 @@ promo_condition_sales AS (
   RIGHT JOIN promo_base_info pbi ON 
     sku.id_common_sku = pbi.id_common_sku AND
     fss.id_client = pbi.id_client
-  LEFT JOIN public.promo_n_plus_x_exception pnpxe ON
+  LEFT JOIN (
+    -- Делаем подзапрос, где получаем id_common_sku из promo_n_plus_x_exception
+    SELECT 
+      pnpxe.*,
+      sku.id_common_sku
+    FROM public.promo_n_plus_x_exception pnpxe
+    JOIN dwh.dim_sku sku ON pnpxe.id_sku = sku.id_sku
+  ) pnpxe ON
     fss.id_client = pnpxe.id_client AND
     fss.tin_buyer = pnpxe.tin_buyer AND
     (
-      (fss.operation_date BETWEEN pnpxe.start_date AND pnpxe.end_date) OR
-      (fss.operation_date BETWEEN pbi.start_date - (pbi.end_date - pbi.start_date) AND pbi.start_date-1)
+      fss.operation_date BETWEEN pnpxe.start_date AND pnpxe.end_date OR
+      fss.operation_date BETWEEN pbi.start_date - (pbi.end_date - pbi.start_date) AND pbi.start_date-1
     ) AND
-    fss.id_sku = pnpxe.id_sku AND
-    fss.id_operation_type = 4
+    sku.id_common_sku = pnpxe.id_common_sku
   WHERE 
     pnpxe.tin_buyer IS NULL AND
+    fss.id_operation_type = 4 AND 
     fss.delete_date = '2000-01-01' AND
     fss.id_client IN (16, 28, 35, 36, 37, 418) AND
     (
       fss.operation_date BETWEEN pbi.start_date AND pbi.end_date OR
       fss.operation_date BETWEEN pbi.start_date - (pbi.end_date - pbi.start_date) AND pbi.start_date-1
-    )
+    ) AND
+    fss.id_operation_type = 4
 ),
 -- Продажи без исключений (для total)
 total_sales AS (
@@ -65,7 +73,7 @@ total_sales AS (
     END AS sales_type
   FROM dwh.fct_sale_second fss
   JOIN dwh.dim_sku sku ON fss.id_sku = sku.id_sku
-  RIGHT JOIN promo_base_info pbi ON 
+  JOIN promo_base_info pbi ON 
     sku.id_common_sku = pbi.id_common_sku AND
     fss.id_client = pbi.id_client
   WHERE 
@@ -125,7 +133,7 @@ agg_table AS (
     SUM(sales_pack) AS sales_pack,
     COUNT(DISTINCT tin_buyer) AS acb
   FROM sales_with_packs
-  WHERE sales_quantity > 0
+  WHERE sales_quantity != 0
   GROUP BY sales_type, id_promotion_type, id_client, start_date, end_date, id_common_sku, id_promotion
 ),
 table_without_price AS (
